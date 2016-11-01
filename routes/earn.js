7bee8dfe788a81e51b6343fd214894a540cea064
Charles Wang <charleswang23103@gmail.com>
2016-10-31 19:16:57 -0700

    fixed the nav bar user profile

--- routes/earn.js
+++ routes/earn.js
@@ -1,21 +1,28 @@
 /*
  * GET home page.
  */
-
-exports.view = function(req, res){
-  var projectName = req.params.projectName;
-  console.log("Project is " + projectName);
-  res.render('earn_page', {
-  	'projectName': projectName
-  });
+var data = require('../data/data.json');
+exports.view = function (req, res) {
+    var projectName = req.params.projectName;
+    console.log("Project is " + projectName);
+    res.render('earn_page', {
+        'projectName': projectName
+    });
 };
-exports.viewEmpty = function(req, res){
-  console.log("Project is emtpy");
-  res.render('earn_page', {
-  	'projectName': ''
-  });
+exports.viewEmpty = function (req, res) {
+    console.log("Project is emtpy");
+    res.render('earn_page', {
+        'projectName': ''
+    });
 };
-exports.nav = function(req,res){
-	console.log("loading the nav bar");
-	res.render('persistent_tab');
+exports.nav = function (req, res) {
+    console.log("loading the nav bar");
+    var profile;
+    for (var i = 0; i < data.table.length; i++) {
+        if (req.session.name == data.table[i].name)
+            profile = data.table[i];
+        console.log(data.table[i].name);
+    }
+    console.log(profile);
+    res.render('persistent_tab', profile);
 }

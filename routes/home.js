e3caf2ec383f6a3d484bcf3b21db9ef64a9ade25
Charles Wang <charleswang23103@gmail.com>
2016-10-31 19:01:20 -0700

    fixed home page

--- routes/home.js
+++ routes/home.js
@@ -1,4 +1,7 @@
+var projects_list = require("../data/projects.json");
+
 exports.view = function (req, res) {
     console.log("loading the nav bar");
-    res.render('home_page');
+
+    res.render('home_page', projects_list);
 }

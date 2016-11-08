98efc43429a28beb1a36b4c796d83b19ade98391
Charles Wang <charleswang23103@gmail.com>
2016-11-07 18:01:06 -0800

    filter redeem page

--- app.js
+++ app.js
@@ -27,6 +27,33 @@ app.engine('handlebars', exphbs({
     helpers: {
         json: function (context) {
             return JSON.stringify(context);
+        },
+        ifCond: function (v1, operator, v2, options) {
+
+            switch (operator) {
+                case '==':
+                    return (v1 == v2) ? options.fn(this) : options.inverse(this);
+                case '===':
+                    return (v1 === v2) ? options.fn(this) : options.inverse(this);
+                case '!=':
+                    return (v1 != v2) ? options.fn(this) : options.inverse(this);
+                case '!==':
+                    return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+                case '<':
+                    return (v1 < v2) ? options.fn(this) : options.inverse(this);
+                case '<=':
+                    return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+                case '>':
+                    return (v1 > v2) ? options.fn(this) : options.inverse(this);
+                case '>=':
+                    return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+                case '&&':
+                    return (v1 && v2) ? options.fn(this) : options.inverse(this);
+                case '||':
+                    return (v1 || v2) ? options.fn(this) : options.inverse(this);
+                default:
+                    return options.inverse(this);
+            }
         }
     }
 }));
--- routes/redeem.js
+++ routes/redeem.js
@@ -2,7 +2,27 @@ var fs = require('fs');
 var rewards = require('../data/rewards.json');
 exports.view = function (req, res) {
     console.log("loading the nav bar");
-    res.render('redeem_page', rewards);
+    fs.readFile('data/data.json', function readFileCallback(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            var obj = JSON.parse(data);
+            console.log(req.body.price);
+            var profile;
+            for (var i = 0; i < obj.table.length; i++) {
+                if (req.session.name == obj.table[i].name) {
+                    profile = obj.table[i];
+                }
+            }
+            var json = {
+                avatars:rewards,
+                profile
+            };
+            console.log(json);
+            res.render('redeem_page', json);
+
+        }
+    });
 }
 
 exports.levelUp = function (req, res, next) {
